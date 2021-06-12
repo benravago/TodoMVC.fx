99e2f81e0d966b37cf28cde2805ea78b4df79fc2
Ben Ravago <ben.ravago@aol.com>
2021-06-12 16:22:38 -0400

    prep

--- src/fxmlc/fx/node/builder/Shim.java
+++
@@ -1,9 +0,0 @@
-package fx.node.builder;
-
-import org.w3c.dom.Element;
-import java.util.function.Function;
-
-public interface Shim {
-    String type();
-    String toString(Element element, Function<String,String> resolver);
-}
